dc04b1b232b2cd74a459511be8a07541f311106a
ankitgyawali <agyaw792@live.kutztown.edu>
2016-02-02 00:29:34 -0500

    readying files for deployment

---
+++ client/configuration.js
@@ -0,0 +1,7 @@
+//Contains global variables used for checksheet manager
+//Authentication. Checks if user is logged in on every route change
+smApp.run(['$rootScope',
+    function($rootScope) {
+ $rootScope.cmTitle = "Kutztown University";
+    	    }
+]);
